08caa9ffc92f90cdf5d79a1952aeb8299d0badf1
Page-David <david991010@gmail.com>
2015-11-14 15:18:37 +0800

    Format change

--- talk-with-robot-webversion/flask_app.py
+++ talk-with-robot-webversion/flask_app.py
@@ -1,29 +1,32 @@
-from flask import Flask,request,render_template
-import requests,urllib.parse
+from flask import Flask, request, render_template
+import requests, urllib.parse
+
 app = Flask(__name__)
-@app.route('/',methods=['GET','POST'])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def robot():
     if request.method == "POST":
-        text=urllib.parse.quote(request.form['text'])
-        url='http://www.tuling123.com/openapi/api?key=4b130e3d0fa202cb8499327de4ce0bcc&info='
-        url+=text
-        r=requests.get(url)
-        print('User send a click,json data is ',r.json())
-        if  r.json()['code'] == 100000:
-            return render_template('robot.html',retext=r.json()['text'])
+        text = urllib.parse.quote(request.form['text'])
+        url = 'http://www.tuling123.com/openapi/api?key=4b130e3d0fa202cb8499327de4ce0bcc&info='
+        url += text
+        r = requests.get(url)
+        print('User send a click,json data is ', r.json())
+        if r.json()['code'] == 100000:
+            return render_template('robot.html', retext=r.json()['text'])
         elif r.json()['code'] == 302000:
-            return render_template('robot.html',retext=r.json()['text'],list=r.json()['list'])
+            return render_template('robot.html', retext=r.json()['text'], list=r.json()['list'])
         elif r.json()['code'] == 200000:
-            return render_template('robot.html',retext=r.json()['text'],reurl=[r.json()['url'],request.form['text']])
+            return render_template('robot.html', retext=r.json()['text'], reurl=[r.json()['url'], request.form['text']])
         elif r.json()['code'] == 305000:
-            return render_template('robot.html',retext=r.json()['text'],retrain=r.json()['list'])
+            return render_template('robot.html', retext=r.json()['text'], retrain=r.json()['list'])
         elif r.json()['code'] == 306000:
-            return render_template('robot.html',retext=r.json()['text'],replane=r.json()['list'])
+            return render_template('robot.html', retext=r.json()['text'], replane=r.json()['list'])
         elif r.json()['code'] == 308000:
-            return render_template('robot.html',retext=r.json()['text'],redish=enumerate(r.json()['list']))
+            return render_template('robot.html', retext=r.json()['text'], redish=enumerate(r.json()['list']))
     else:
         return render_template('robot.html')
 
-if __name__=='__main__':
-    app.run()
 
+if __name__ == '__main__':
+    app.run()
